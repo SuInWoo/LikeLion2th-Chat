9832c48b0292d5c99b198e4ab3a59bdff3d030f6
SuInWoo <wbo1026@naver.com>
2022-12-19 11:25:15 +0900

    Get Messages

--- src/main/java/com/exam/appspringboot/controller/ChatController.java
+++ src/main/java/com/exam/appspringboot/controller/ChatController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.IntStream;
 
 @Controller
 @RequestMapping("/chat")
@@ -34,16 +35,36 @@ public class ChatController {
         );
     }
 
-    public record MessageResponse(List<ChatMessage> messages, long count) {
+    public record MessagesRequest(Long fromId) {
+    }
+
+    public record MessagesResponse(List<ChatMessage> messages, long count) {
     }
 
     @GetMapping("/messages")
     @ResponseBody
-    public RsData<MessageResponse> messages() {
+    public RsData<MessagesResponse> messages(MessagesRequest req) {
+        List<ChatMessage> messages = chatMessages;
+
+        // 번호가 입력되었다면
+        if (req.fromId != null) {
+            // 해당 번호의 채팅메세지가 전체 리스트에서의 배열인덱스 번호를 구한다.
+            // 없다면 -1
+            int index = IntStream.range(0, messages.size())
+                    .filter(i -> chatMessages.get(i).getId() == req.fromId)
+                    .findFirst()
+                    .orElse(-1);
+
+            if (index != -1) {
+                // 만약에 index가 있다면, 0번 부터 index 번 까지 제거한 리스트를 만든다.
+                messages = messages.subList(index + 1, messages.size());
+            }
+        }
+
         return new RsData<>(
                 "S-1",
                 "성공",
-                new MessageResponse(chatMessages, chatMessages.size())
+                new MessagesResponse(messages, messages.size())
         );
     }
 
