1cfc4566b41c1c9f2a3a8956a3f2d8de0724a130
SuInWoo <wbo1026@naver.com>
2022-12-19 10:53:34 +0900

    Create Message and Get

--- src/main/java/com/exam/appspringboot/controller/ChatController.java
+++ src/main/java/com/exam/appspringboot/controller/ChatController.java
@@ -1,18 +1,47 @@
 package com.exam.appspringboot.controller;
 
+import com.exam.appspringboot.RsData;
+import com.exam.appspringboot.entity.ChatMessage;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 @RequestMapping("/chat")
 public class ChatController {
 
+    private List<ChatMessage> chatMessages = new ArrayList<>();
+
+    public record WriteMessageRequest(String authorName, String content) {
+    }
+
+    public record WriteMessageResponse(long id) {
+    }
+
     @PostMapping("/writeMessage")
     @ResponseBody
-    public String writeMessage() {
-        return "메세지가 작성되었습니다.";
+    public RsData<WriteMessageResponse> writeMessage(@RequestBody WriteMessageRequest req) {
+        ChatMessage message = new ChatMessage(req.authorName(), req.content());
+
+        chatMessages.add(message);
+
+        return new RsData<>(
+                "S-1",
+                "메세지가 작성되었습니다.",
+                new WriteMessageResponse(message.getId())
+        );
+    }
+
+    @GetMapping("/messages")
+    @ResponseBody
+    public RsData<List<ChatMessage>> messages() {
+        return new RsData<>(
+                "S-1",
+                "성공",
+                chatMessages
+        );
     }
 
 }
