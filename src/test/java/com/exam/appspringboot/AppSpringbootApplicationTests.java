a128674d3d8f2d3209dcf1b8329b41b50977927b
SuInWoo <wbo1026@naver.com>
2022-12-19 10:27:56 +0900

    first commit

---
+++ src/test/java/com/exam/appspringboot/AppSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exam.appspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
